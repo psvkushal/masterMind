a189ae9238eb40d701a476b21c33d0fe6323ea38
psvkushal <psvkushal@gmail.com>
2023-06-26 22:50:52 +0530

    Adds the readme file for tracking the project info and code in game.rb

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # masterMind
-Master Mind game written in ruby
+
+## Part 1 Computer as the answer generator
+
+* answer_generator function for the computer to create the answer
+  * The answer would be stored in an array 
+  * The numbers will be generated randomly with numbers being between maximum and minimum number
+* function to take in the input from the user to take a guess
+* function to compare between the guess and answer
+  * The function should return 
+    * 1 red circle for every digit present in the answer with position same as in the answer
+    * 1 white circle for every digit present in the answer but not in the same posistion as in the answer
+  * For implementaion purpose I can simply go for 1-1 comparision between answer and guess, keep track of indices which are not matched during the comparision and look for if those digits corresponding to that indices are present in the guess.
+  * The first one should give the number of red circles and the second one should give the white
+* There would be limit on the number of guess the player can take
+* Print rules at the start for understanding the game since not everyone might have played the game (like myself)
+* The answer would be stored in a class variable (remeber in ruby class variable is starts with @@ if there is not attr_accessor then you would have to keep @@ at every place)
--- game.rb
+++ game.rb
@@ -1,28 +1,60 @@
 class GameBoard
-  attr_accessor answer
+  attr_accessor :answer
 
   RED_CIRCLE="\u{1F534}"
   WHITE_CIRCLE="\u{26AA}"
   BLACK_CIRCLE="\u{26ab}"
 
   ANWSER_LEN=4
-  DIGIT_UPPER_LIMIT=9
+  DIGIT_UPPER_LIMIT=6
   DIGIT_LOWER_LIMIT=0
 
+  @@answer = Array.new
+  puts @@answer.class 
+  def getGuess()
+    puts "please enter the guess number the the digit can be between #{DIGIT_LOWER_LIMIT} and #{DIGIT_UPPER_LIMIT} and the length of the answer should be #{ANWSER_LEN}"
+    isCorrectForm = false 
+    
+    while !isCorrectForm
+      guess = gets.chomp
+      if guess.length != ANWSER_LEN
+        puts "The size of the guess should be #{ANWSER_LEN}"
+        next
+      end
+      isCorrectForm = true
+      number_list = guess.split("")
+      for num in number_list
+        if (Integer(num) < DIGIT_LOWER_LIMIT) | (Integer(num) > DIGIT_UPPER_LIMIT)
+          isCorrectForm = false
+          puts "The numbers should be in the range of #{DIGIT_LOWER_LIMIT} and #{DIGIT_UPPER_LIMIT}"
+        end
+        if !isCorrectForm
+          break
+        end
+      end
+    end
+
+    int_guess = Array.new
+    number_list.each{|x| int_guess.push(x)}
+    return int_guess 
+  end
 
   def generate_answer()
     for i in 1..ANWSER_LEN
-      answer.push(rand(DIGIT_LOWER_LIMIT..DIGIT_UPPER_LIMIT))
+      @@answer.push(rand(DIGIT_LOWER_LIMIT..DIGIT_UPPER_LIMIT))
     end
+    return @@answer
   end
 
-  def compare_answer(answer, guess)
+  def compare_answer(guess)
+     
   end
 
   def print_test()
     #printing to check if the emojis are getting printed properly
     puts RED_CIRCLE + WHITE_CIRCLE + BLACK_CIRCLE
-    puts answer
+    puts @@answer.class
+    puts @@answer
     puts generate_answer()
   end
 
@@ -30,3 +62,4 @@ end
 
 game = GameBoard.new
 game.print_test
+puts game.getGuess
